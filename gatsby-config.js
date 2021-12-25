4b20480d6aee88c70df2018c6dd6842c305ba09c
ritik agrawal <ritikagrawal2710@gmail.com>
2021-12-25 19:24:33 +0530

    added my details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ritik Blogs`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ritik Agrawal`,
+      summary: `who lives and works in Mumbai(India) building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
